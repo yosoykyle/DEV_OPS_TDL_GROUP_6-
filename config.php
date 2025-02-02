7815f7669524edb11a0263de3a3c9ffbe822bd13
kyle <141384233+yosoykyle@users.noreply.github.com>
2025-02-02 18:00:13 +0800

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,6 @@ $user = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
 
 try {
-    // Create a PDO connection string for PostgreSQL
     $dsn = "pgsql:host=$host;port=5432;dbname=$dbname";
     $conn = new PDO($dsn, $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
